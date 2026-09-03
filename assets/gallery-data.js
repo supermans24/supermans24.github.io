8c02bb8e0cf0b2d7aaa0d1cbb48f6e0b67a78177
supermans <supermans@local>
2026-09-03 14:31:27 +0900

    슈퍼맨즈 주방·집수리 홈페이지 v3

---
+++ assets/gallery-data.js
@@ -0,0 +1,324 @@
+const GALLERY = [
+{
+"c": "robot",
+"t": "김해 창원 드리미 로봇청소기 오븐장 아래 서랍 활용, 직배수 연결까지",
+"d": "2026. 9. 1.",
+"u": "https://blog.naver.com/supermans8157/224397349702",
+"img": "assets/gallery/robot/224397349702_0.jpg"
+},
+{
+"c": "robot",
+"t": "로봇청소기 직배수 설치, 싱크대장 리폼 전 확인해야 할 조건",
+"d": "2026. 8. 28.",
+"u": "https://blog.naver.com/supermans8157/224392925132",
+"img": "assets/gallery/robot/224392925132_0.jpg"
+},
+{
+"c": "robot",
+"t": "진해 ㄱ자 주방, 로봇청소기 직배수를 위한 밥솥장 리폼 후기",
+"d": "2026. 8. 24.",
+"u": "https://blog.naver.com/supermans8157/224388377061",
+"img": "assets/gallery/robot/224388377061_0.jpg"
+},
+{
+"c": "robot",
+"t": "양산 싱크대리폼, 키큰장 아래 로보락 S10 급수 세척 자동화 공간 만들기",
+"d": "2026. 8. 23.",
+"u": "https://blog.naver.com/supermans8157/224387793616",
+"img": "assets/gallery/robot/224387793616_0.jpg"
+},
+{
+"c": "robot",
+"t": "대구 로봇청소기장 만들기｜광파오븐 아래 로보락 S10 직배수 설치",
+"d": "2026. 8. 17.",
+"u": "https://blog.naver.com/supermans8157/224380340117",
+"img": "assets/gallery/robot/224380340117_0.jpg"
+},
+{
+"c": "robot",
+"t": "울산 로봇청소기 LG 직배수장 리폼｜로청장 설치를 위한 싱크대 리폼",
+"d": "2026. 8. 12.",
+"u": "https://blog.naver.com/supermans8157/224376219679",
+"img": "assets/gallery/robot/224376219679_0.jpg"
+},
+{
+"c": "robot",
+"t": "대구 로봇청소기 로보락 S10 울트라 가스레인지 아래 설치",
+"d": "2026. 8. 10.",
+"u": "https://blog.naver.com/supermans8157/224374483101",
+"img": "assets/gallery/robot/224374483101_0.png"
+},
+{
+"c": "robot",
+"t": "울산 로봇청소기 직배수장 리폼｜로보락 S10 밥솥장 활용",
+"d": "2026. 8. 9.",
+"u": "https://blog.naver.com/supermans8157/224373040101",
+"img": "assets/gallery/robot/224373040101_0.png"
+},
+{
+"c": "robot",
+"t": "경주 로봇청소기장 리폼- 광파오븐 하부장 활용 로보락 사로스(z70)",
+"d": "2026. 8. 6.",
+"u": "https://blog.naver.com/supermans8157/224369306628",
+"img": "assets/gallery/robot/224369306628_0.png"
+},
+{
+"c": "robot",
+"t": "김해 로보락 로봇청소기 장리폼 더샵신문그리니티 키큰장 활용",
+"d": "2026. 8. 5.",
+"u": "https://blog.naver.com/supermans8157/224367695784",
+"img": "assets/gallery/robot/224367695784_0.png"
+},
+{
+"c": "reform",
+"t": "대구 빌트인 식기세척기·오븐 철거 후 하부장 설치 작업",
+"d": "2026. 8. 22.",
+"u": "https://blog.naver.com/supermans8157/224386924381",
+"img": "assets/gallery/reform/224386924381_0.jpg"
+},
+{
+"c": "reform",
+"t": "양산 싱크대 리폼 한 번에 해결! 오븐 철거·벽 복원·싱크볼 교체·상판 연마",
+"d": "2026. 8. 20.",
+"u": "https://blog.naver.com/supermans8157/224384731797",
+"img": "assets/gallery/reform/224384731797_0.jpg"
+},
+{
+"c": "reform",
+"t": "양산 싱크볼 떨어짐 수리｜개수대 교체 없이 재부착 및 밑판 교체",
+"d": "2026. 8. 14.",
+"u": "https://blog.naver.com/supermans8157/224378940755",
+"img": "assets/gallery/reform/224378940755_0.jpg"
+},
+{
+"c": "reform",
+"t": "창원 식기세척기 설치를 위한 공간 확보- 높이가 부족해요",
+"d": "2026. 8. 5.",
+"u": "https://blog.naver.com/supermans8157/224369334020",
+"img": "assets/gallery/reform/224369334020_0.png"
+},
+{
+"c": "reform",
+"t": "부산 식기세척기 철거 후 하부장 설치｜싱크대 상판 금간 곳 보수",
+"d": "2026. 8. 2.",
+"u": "https://blog.naver.com/supermans8157/224365944015",
+"img": "assets/gallery/reform/224365944015_0.jpg"
+},
+{
+"c": "reform",
+"t": "울산 냉장고장 리폼, 가운데 중간판 제거 후 틈새장 설치로 수납까지 해결!",
+"d": "2026. 7. 30.",
+"u": "https://blog.naver.com/supermans8157/224360956503",
+"img": "assets/gallery/reform/224360956503_0.jpg"
+},
+{
+"c": "reform",
+"t": "김해 싱크대 식기세척기 철거 후 서랍장 리폼 가능한가요?",
+"d": "2026. 7. 27.",
+"u": "https://blog.naver.com/supermans8157/224359300296",
+"img": "assets/gallery/reform/224359300296_0.jpg"
+},
+{
+"c": "reform",
+"t": "울산 싱크대상판 연마광택, 사각싱크볼 교체로 새 주방처럼 달라졌습니다",
+"d": "2026. 7. 21.",
+"u": "https://blog.naver.com/supermans8157/224352883576",
+"img": "assets/gallery/reform/224352883576_0.jpg"
+},
+{
+"c": "crack",
+"t": "부산 싱크대 상판 금이 길게 번졌어요｜인조대리석 크랙 수리 후기",
+"d": "15시간 전",
+"u": "https://blog.naver.com/supermans8157/224389661747",
+"img": "assets/gallery/crack/224389661747_0.jpg"
+},
+{
+"c": "crack",
+"t": "싱크대 상판에 금이 갔다면? 부산 크랙 복원 시공 사례",
+"d": "2026. 9. 1.",
+"u": "https://blog.naver.com/supermans8157/224388354132",
+"img": "assets/gallery/crack/224388354132_0.jpg"
+},
+{
+"c": "crack",
+"t": "부산 싱크볼 옆 실금 크랙, 누수 전 인조대리석 상판 수리 후기",
+"d": "2026. 8. 7.",
+"u": "https://blog.naver.com/supermans8157/224371139875",
+"img": "assets/gallery/crack/224371139875_0.png"
+},
+{
+"c": "crack",
+"t": "울산 싱크대 상판 갈라짐 수리｜측면 벌어짐·식세기 위 크랙 보수",
+"d": "2026. 8. 3.",
+"u": "https://blog.naver.com/supermans8157/224365961549",
+"img": "assets/gallery/crack/224365961549_0.jpg"
+},
+{
+"c": "crack",
+"t": "김해 인조대리석 상판 금 수리｜율하 시티프라디움 조각심기 복원 사례",
+"d": "2026. 7. 31.",
+"u": "https://blog.naver.com/supermans8157/224363280721",
+"img": "assets/gallery/crack/224363280721_0.jpg"
+},
+{
+"c": "crack",
+"t": "김해 싱크대상판 균열 복원, 충격으로 생긴 T자형 파손 수리 후기",
+"d": "2026. 7. 29.",
+"u": "https://blog.naver.com/supermans8157/224360777233",
+"img": "assets/gallery/crack/224360777233_0.jpg"
+},
+{
+"c": "crack",
+"t": "부산 싱크대상판 점점 번지는 실금 수리 복원해 보신분 계실까요?",
+"d": "2026. 7. 20.",
+"u": "https://blog.naver.com/supermans8157/224352554180",
+"img": "assets/gallery/crack/224352554180_0.jpg"
+},
+{
+"c": "crack",
+"t": "부산 싱크대 상판 크랙 수리, 교체 없이 복원 가능할까요?",
+"d": "2026. 7. 16.",
+"u": "https://blog.naver.com/supermans8157/224348153895",
+"img": "assets/gallery/crack/224348153895_0.jpg"
+},
+{
+"c": "hood_sink",
+"t": "부산 주방후드 켜도 냄새가 남아요｜소음 심한 하츠 침니후드 교체",
+"d": "16시간 전",
+"u": "https://blog.naver.com/supermans8157/224391993123",
+"img": "assets/gallery/hood_sink/224391993123_0.jpg"
+},
+{
+"c": "hood_sink",
+"t": "싱크볼 교체 고민이라면? 경주 벨라고 세레니티+에떼르노 EK3001 교체 시공 사례",
+"d": "2026. 8. 25.",
+"u": "https://blog.naver.com/supermans8157/224389591812",
+"img": "assets/gallery/hood_sink/224389591812_0.jpg"
+},
+{
+"c": "hood_sink",
+"t": "부산 사각싱크볼 교체｜아티잔 네오녹스 RT858 5면 엠보 설치 후기",
+"d": "2026. 8. 16.",
+"u": "https://blog.naver.com/supermans8157/224380326899",
+"img": "assets/gallery/hood_sink/224380326899_0.jpg"
+},
+{
+"c": "hood_sink",
+"t": "창원 싱크대 아티잔 PEDU858 엠보코팅 사각싱크볼 교체, 루미노바 LV18000 수전 설치",
+"d": "2026. 8. 11.",
+"u": "https://blog.naver.com/supermans8157/224373942171",
+"img": "assets/gallery/hood_sink/224373942171_0.png"
+},
+{
+"c": "hood_sink",
+"t": "김해 싱크볼 교체 - 아티잔EDU858, 에떼르노 4way",
+"d": "2026. 8. 10.",
+"u": "https://blog.naver.com/supermans8157/224373376348",
+"img": "assets/gallery/hood_sink/224373376348_0.png"
+},
+{
+"c": "hood_sink",
+"t": "울산 사각싱크볼 교체 - 백조 CESWSR8635",
+"d": "2026. 8. 4.",
+"u": "https://blog.naver.com/supermans8157/224367646101",
+"img": "assets/gallery/hood_sink/224367646101_0.png"
+},
+{
+"c": "hood_sink",
+"t": "양산 사각싱크볼 교체 추천｜세라믹 상판에 아티잔 EDU858, 에떼르노 3001 설치",
+"d": "2026. 7. 31.",
+"u": "https://blog.naver.com/supermans8157/224363257505",
+"img": "assets/gallery/hood_sink/224363257505_0.jpg"
+},
+{
+"c": "hood_sink",
+"t": "부산 사각싱크볼 교체 저렴한 곳 알려주세요",
+"d": "2026. 7. 24.",
+"u": "https://blog.naver.com/supermans8157/224356912425",
+"img": "assets/gallery/hood_sink/224356912425_0.jpg"
+},
+{
+"c": "polish",
+"t": "부산 현관 디딤석 대리석 복원｜뿌연 변색·얼룩 제거부터 연마 코팅까지",
+"d": "2026. 8. 27.",
+"u": "https://blog.naver.com/supermans8157/224391958174",
+"img": "assets/gallery/polish/224391958174_0.jpg"
+},
+{
+"c": "polish",
+"t": "싱크대 교체없이 연마광택으로 깨끗하게, 비용은?",
+"d": "2026. 8. 19.",
+"u": "https://blog.naver.com/supermans8157/224383367748",
+"img": "assets/gallery/polish/224383367748_0.jpg"
+},
+{
+"c": "polish",
+"t": "부산 해운대 싱크대 상판 연마광택 - 오래된 인조대리석 깨끗하게 복원",
+"d": "2026. 8. 16.",
+"u": "https://blog.naver.com/supermans8157/224380365445",
+"img": "assets/gallery/polish/224380365445_0.jpg"
+},
+{
+"c": "polish",
+"t": "양산 싱크대 상판 연마광택, 시공 전후 비교 후기",
+"d": "2026. 7. 9.",
+"u": "https://blog.naver.com/supermans8157/224341617728",
+"img": "assets/gallery/polish/224341617728_0.jpg"
+},
+{
+"c": "polish",
+"t": "부산 롯데캐슬 싱크대 상판 연마광택, 사용감은 줄이고 광택은 되살리기",
+"d": "2026. 7. 7.",
+"u": "https://blog.naver.com/supermans8157/224337952616",
+"img": "assets/gallery/polish/224337952616_0.jpg"
+},
+{
+"c": "polish",
+"t": "울산 사각싱크볼교체 연마광택으로 상판 스크래치까지 복원",
+"d": "2026. 7. 3.",
+"u": "https://blog.naver.com/supermans8157/224335684367",
+"img": "assets/gallery/polish/224335684367_0.jpg"
+},
+{
+"c": "cutting",
+"t": "경주 인덕션 교체, 기존 타공 상판 메꿈 및 재가공 후기",
+"d": "2026. 7. 7.",
+"u": "https://blog.naver.com/supermans8157/224337905023",
+"img": "assets/gallery/cutting/224337905023_0.jpg"
+},
+{
+"c": "cutting",
+"t": "부산 싱크대상판 정수기 홀 타공 막음",
+"d": "2026. 6. 2.",
+"u": "https://blog.naver.com/supermans8157/224303221395",
+"img": "assets/gallery/cutting/224303221395_0.jpg"
+},
+{
+"c": "cutting",
+"t": "양산 아트월 구멍복원 티비설치 흔적 지우기 메꿈",
+"d": "2025. 11. 7.",
+"u": "https://blog.naver.com/supermans8157/224067249598",
+"img": "assets/gallery/cutting/224067249598_0.jpg"
+},
+{
+"c": "cutting",
+"t": "울산 경주 인덕션 대리석 절단과 갈라짐 수리하기",
+"d": "2025. 10. 26.",
+"u": "https://blog.naver.com/supermans8157/224050852923",
+"img": "assets/gallery/cutting/224050852923_0.jpg"
+},
+{
+"c": "cutting",
+"t": "부산 김해 싱크대 상판 타공 가스레인지설치",
+"d": "2025. 10. 13.",
+"u": "https://blog.naver.com/supermans8157/224037897787",
+"img": "assets/gallery/cutting/224037897787_0.jpg"
+},
+{
+"c": "cutting",
+"t": "울산 주방 정수기 철거 후 구멍 막음",
+"d": "2025. 10. 9.",
+"u": "https://blog.naver.com/supermans8157/224032434134",
+"img": "assets/gallery/cutting/224032434134_0.jpg"
+}
+];
